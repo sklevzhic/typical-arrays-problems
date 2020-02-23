2018b018288839a66a6b21db9d4eb56fe80ae5d6
Alexander Klevzhits <sklevzhic@gmail.com>
2020-02-24 00:00:20 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    array.sort(function (a,b) {
+        return a - b
+    });
+  return array[0];
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    array.sort(function (a,b) {
+        return b - a;
+    });
+    return array[0];
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  return array.reduce((a,b) => (a + b ))/ array.length;
+};
