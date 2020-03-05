78f6190bc7399757d8646e2aebea99aabe309dd8
Alexander Klevzhits <sklevzhic@gmail.com>
2020-03-05 23:36:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 
 exports.min = function min (array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
     array.sort(function (a,b) {
         return a - b
     });
@@ -7,6 +10,9 @@ exports.min = function min (array) {
 };
 
 exports.max = function max (array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
     array.sort(function (a,b) {
         return b - a;
     });
@@ -14,5 +20,8 @@ exports.max = function max (array) {
 };
 
 exports.avg = function avg (array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
   return array.reduce((a,b) => (a + b ))/ array.length;
 };
